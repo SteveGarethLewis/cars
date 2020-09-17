1e7ddef19072f105f2e40c6cacdef7da19a5cd20
Steve Lewis <steve.lewis@elasticstage.com>
2020-09-17 10:04:26 +0100

    Bootstrap frontend. Adds route for cars. Fetches data from endpoint using axios

---
+++ src/views/Cars.vue
@@ -0,0 +1,34 @@
+<template>
+    <div v-if="loaded">
+        {{ cars }}
+    </div>
+    <div v-else>
+        Loading
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: "Cars",
+
+    data () {
+        return {
+            loaded: false,
+            cars: []
+        }
+    },
+
+    created () {
+        this.fetchCars().then(() => this.loaded = true)
+    },
+
+    methods: {
+        async fetchCars () {
+            axios.get('https://demo-api.getmygrades.co.uk/cars')
+                .then(response => this.cars = response.data)
+        }
+    }
+}
+</script>
